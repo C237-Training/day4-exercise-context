175fd48071485212fce52b41e6c996b92ad0c006
jason-lim <jason_lim@rp.edu.sg>
2023-06-22 16:22:45 +0800

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import ShowReviews from "./ShowReviews";
+import AddReview from "./AddReview";
+import EditReview from "./EditReview";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ReviewProvider } from './ReviewContext';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [editIndex, setEditIndex] = useState(-1); 
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    setPage('edit');
+  }
+
+  const renderPage = () => {
+    switch (page) {
+      case "show":
+        return <ShowReviews onEdit={handleEdit} />
+      case "add":
+        return <AddReview changePage={changePage} />
+      case "edit":
+        return <EditReview index={editIndex}
+          changePage={changePage} />
+      default:
+        return ""
+    }
+  }
+
+
+  const [page, setPage] = useState('show')
+  const changePage = (newPage) => {
+    setPage(newPage)
+  }
+
+  return <>
+        <nav className="navbar navbar-expand-sm bg-light">
+            <div className="container-fluid">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#" onClick={()=>{
+                            changePage('show')
+                        }}>All Reviews</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#" onClick={()=>{
+                            changePage('add')
+                        }}>Add New</a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+
+        <div className="container">
+            <ReviewProvider>
+            {
+                renderPage()
+            }
+            </ReviewProvider>
+        </div>
+    </>
+
+
 }
 
 export default App;
